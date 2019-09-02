bf883632e347982af80d782770dfe7c16f214af2
Maxi Ferreira <charca@gmail.com>
2019-09-02 16:19:31 -0300

    Updated Message Preview experiment

---
+++ src/components/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default ({ children }) => (
+  <div
+    style={{
+      backgroundColor: '#cce5ff',
+      border: '1px solid #b8daff',
+      borderRadius: '.25rem',
+      color: '#004085',
+      padding: '.75rem 1.25rem',
+    }}
+  >
+    {children}
+  </div>
+)
